91bb28f70f27ca552f3c07e3145504c40fb26c99
GJ <lgj9172@gmail.com>
2024-12-01 20:28:06 +0900

    add: dependency

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    config.externals = [...config.externals, { canvas: "canvas" }]; // canvas 모듈을 외부 의존성으로 처리
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
